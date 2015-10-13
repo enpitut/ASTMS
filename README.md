9dfc874e2df15c173b6d87531bd963e2ada612d2
Shiohata <15nm710t@vc.ibaraki.ac.jp>
2015-10-13 16:46:57 +0900

    READMEに導入方法を追記

--- README.md
+++ README.md
@@ -1,26 +1,68 @@
-## ASTMIS
-###チーム
-####メンバー
+#ASTMS
+##メンバー
 * 戸田 敬太
 * 宋双宝
 * 味岡 孝昇
 * 宮城 優里
 * 塩畑 博文
 
-###プロダクト
-####プロダクト名
+##プロダクト
+###プロダクト名
 Shop Area Wiki
 
-####エレベーターピッチ
+###エレベーターピッチ
 [売り場が見つからないこと]を解決したい  
 [店に初めて訪れる人]や[売り場がわかる・店の地図共有]ができる  
 [Shop Area Wiki]です  
 [店内マップ作成・共有]ができ  
 [店員に聞くこと]とは違って  
-[どこでも使える]  
+[どこでも使える]
 
-###名言
-Inspiration comes through deep reflection.
+##導入方法
+MAMPまたはXAMPPを使用していることが前提です
 
-###名言
-It’s fine to celebrate success but it is more important to heed the lessons of failure.
+1. このリポジトを適当な場所にクローン
+	`$ git clone https://github.com/enpitut/WakeUp.git`  
+	このディレクトリは Web サーバからアクセスできるようにする必要があります。
+	
+1. webrootの設定
+	1. app/ShopAreaWiki\_webroot ディレクトリ をhtdocsに移動  
+		
+	1. ShopAreaWiki\_webroot/index.phpの40行目を編集  
+		クローンしたディレクトリのパスを記入してください。  
+		例えば /Users/hoge/ASTMS の場合は以下のようになります。  
+		`define('ROOT', DS.'Users'.DS.'hoge'.DS.'ASTMS');`  
+
+	1. ShopAreaWiki\_webrootのディレクトリ名をShopAreaWikiに変更  
+1. データベースの作成
+	document/design\_specifications/shop\_area\_wiki.sqlを使ってShopAreaWikiのデータベースを作成
+	
+	
+1. app/Config/database.phpの編集  
+	データベースの情報を変更
+
+		`public $default = array(
+			'datasource' => 'Database/Mysql',
+			'persistent' => false,
+			'host' => 'localhost',
+			'login' => 'root',
+			'password' => 'hogehoge',
+			'database' => 'shop_area_wiki',
+			'prefix' => '',
+			//'encoding' => 'utf8',
+		);
+		
+		public $test = array(
+			'datasource' => 'Database/Mysql',
+			'persistent' => false,
+			'host' => 'localhost',
+			'login' => 'user',
+			'password' => 'password',
+			'database' => 'test_database_name',
+			'prefix' => '',
+			//'encoding' => 'utf8',
+		);`
+
+1. 後はcakePHP 2.x と同じ使い方で使えます
+	例えば、localサーバを立ち上げshopの検索画面を表示する場合は、  
+	http://localhost/ShopAreaWiki/Shopsに移動してください
--- document/design_specifications/shop_area_wiki.sql
+++ document/design_specifications/shop_area_wiki.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 2015 年 8 月 26 日 18:44
+-- Generation Time: 2015 年 10 月 13 日 16:29
 -- サーバのバージョン： 5.5.38-log
 -- PHP Version: 5.6.2
 
@@ -50,7 +50,7 @@ INSERT INTO `blocks` (`id`, `shop_id`, `floor_id`, `block_id`, `x-coordinate`, `
 (2, 2, 1, NULL, 1, 2, 0, '本', 'テストコメント', NULL, NULL, NULL),
 (3, 2, 1, NULL, 2, 2, 0, '本', 'テストコメント', NULL, NULL, NULL),
 (4, 2, 1, NULL, 3, 2, 0, '本', 'テストコメント', NULL, NULL, NULL),
-(5, 2, 1, NULL, 2, 1, 0, '本', 'テストコメント', NULL, NULL, NULL),
+(5, 2, 1, NULL, 2, 1, 0, '味', 'テストコメント', NULL, NULL, NULL),
 (6, 2, 1, NULL, 3, 1, 0, '本', 'テストコメント', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
@@ -73,7 +73,8 @@ CREATE TABLE `floors` (
 
 INSERT INTO `floors` (`id`, `shop_id`, `name`, `created`, `modified`) VALUES
 (1, 2, 'イオン内原1階', NULL, NULL),
-(2, 2, 'イオン内原2階', NULL, NULL);
+(2, 2, 'イオン内原2階', NULL, NULL),
+(3, 50, 'イオン東根1階', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,7 +92,7 @@ CREATE TABLE `shops` (
   `comment` varchar(200) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
 
 --
 -- テーブルのデータのダンプ `shops`
@@ -99,7 +100,10 @@ CREATE TABLE `shops` (
 
 INSERT INTO `shops` (`id`, `name`, `postal_code`, `prefecture`, `street_address`, `category`, `comment`, `created`, `modified`) VALUES
 (1, 'イオンつくば店', '321-1111', '茨城県', 'つくば市天王台', '大型デパート', 'なんでもうっている', NULL, NULL),
-(2, 'イオン 内原', '213-1212', 'apple', '水戸市内原', '小売店', 'でかい！', '2015-08-26 10:12:05', '2015-08-26 10:12:05');
+(2, 'イオン 内原', '213-1212', '茨城県', '水戸市内原', 'デパート', 'でかい！', '2015-08-26 10:12:05', '2015-08-26 10:12:05'),
+(50, 'イオン東根店', '999-3720', '山口県', '東根市 さくらんぼ駅前三丁目', '大型デパート', '駅前です', '2015-08-27 13:29:50', '2015-08-27 13:29:50'),
+(57, 'イーアスつくば', '305-0817', '茨城県', 'つくば市 研究学園５丁目１９番', 'デパート', 'すべてのあなたに、良い明日を。"をコンセプトにお客さまの暮らしを豊かにするモノ・コト・サービスをご提供します。', '2015-08-30 17:08:23', '2015-08-30 17:08:23'),
+(58, 'テスト', 'where', '茨城県', 'd', 'sdfs', 's', '2015-10-09 15:27:09', '2015-10-09 15:27:09');
 
 --
 -- Indexes for dumped tables
@@ -141,7 +145,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `shops`
 --
 ALTER TABLE `shops`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=59;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
