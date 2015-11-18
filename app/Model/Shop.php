7f28958e5769a9df5ad2d3be269c60c1273e51f1
miyavanilla <miyagi.yuri@is.ocha.ac.jp>
2015-11-18 16:16:23 +0900

    バリデーション追加
    ・店舗名…入力必須
    ・郵便番号…空か”123-4567”形式での入力のみ受付
    ・住所…入力必須

--- app/Model/Shop.php
+++ app/Model/Shop.php
@@ -2,5 +2,32 @@
 class Shop extends AppModel {
 	public $hasMany = 'Floor';
 
+	//バリデーション
+	public $validate = array(
+		//ショップ名
+		'name' => array(
+    			'notEmpty' => array(
+      				'rule'    => 'notEmpty',
+      				'allowEmpty' => true,
+    			)
+ 	 	),
+
+ 	 	//郵便番号
+ 	 	'postal_code' => array(
+    			'notEmpty' => array(
+      				'rule' => array( 'custom', '/^[0-9]{3}-[0-9]{4}$/'),//ハイフン必須
+      				//'rule' => array( 'custom', '/^[0-9]{3}[\s-]?[0-9]{4}$/'),//ハイフンあってもなくてもOK
+      				'allowEmpty' => true,
+    			)
+ 	 	),
+
+ 	 	//住所
+ 	 	'street_address' => array(
+    			'notEmpty' => array(
+      				'rule'    => 'notEmpty',
+      				'allowEmpty' => false,
+    			)
+ 	 	),
+	);
 }
 ?>
